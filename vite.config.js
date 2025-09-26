2530205ca0b9dbb937363952e19b27d7b75d1d31
Tsubasa Yumura <yumu@juice.local>
2025-09-26 12:50:55 +0900

    表記ミスを修正

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()], 
 	base: '/mcc-website2025/',
 });
