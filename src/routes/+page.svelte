22d3fec47e8a3e5ec9b353c0dcfa46d3d1c80e20
Tukimiya0317 <dod32013@gmail.com>
2025-12-20 11:37:31 +0900

    スマホ版とタブレット版のレスポンシブ追加とぐるぐる回る箇所の文字変更、その他こまごまとしたものと画像がうまく反映されないので修正中

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 <!-- ヘッター部分・メインTOP画像-->
 
 <section class="TOP">
-    <img src="MCC/top.png" alt="MCC外観" class="TOP__img" />
+    <img src="{base}/MCC/top.png" alt="MCC外観" class="TOP__img" />
     <div class="TOP__overlay"></div>
 
   <!-- 装飾図形-->
@@ -23,7 +23,7 @@
   <div class="circle3"></div>
   <div class="circle4"></div>
   <div class="circle5"></div>
-  <img src="MCC/zukei01.png" alt="図形" class="zukei__img" />
+  <img src="{base}/MCC/zukei01.png" alt="図形" class="zukei__img" />
 
   <div class="TOP__inner">
   <h1 class="TOP__title russo">Media<br/>Creative<br/>Center</h1>
@@ -92,21 +92,21 @@
   const facilities = [
     { no: '01', title: '共創エリア',
       body: '創作活動を行うためのスペース。ハイスペックPCが常設され、映像制作やWeb制作などのクリエイティブな作業に活用できます。ディスカッションスペースを活用してグループワークを進めることもできます。',
-      img: 'MCC/Image01.png' },
+      img: '${base}/MCC/Image01.png' },
     { no: '02', title: 'サウンドアートラボ',
       body: '楽曲制作や音源収録などの音楽活動を行うためのスペース。音楽関連の機材が設置されています。利用には、MCC利用申請後に別途登録作業が必要です',
-      img: 'MCC/Image02.png' },
+      img: '${base}/MCC/Image02.png' },
     { no: '03', title: 'テックラボ',
       body: 'はんだづけなどの電子工作を行うためのスペース。はんだごてやブレッドボードなど、電子回路のプロトタイピングに必要な機材が設置されています。利用には、MCC利用申請後に別途登録作業が必要です。',
-      img: 'MCC/Image03.png' }
+      img: '${base}/MCC/Image03.png' }
   ];
 
   const related = [
-    { title: 'モーションキャプチャー', img: 'MCC/Image04.png',
+    { title: 'モーションキャプチャー', img: '${base}/MCC/Image04.png',
       body: 'VICON社製のマーカレスモーションキャプチャを設置。映画、ゲーム制作、研究に活用できます。北海道情報技術研究所 1階 大スタジオ内にあります。' },
-    { title: 'Gスタジオ', img: 'MCC/PXL_20250613_050406028 1.png',
+    { title: 'Gスタジオ', img: '${base}/MCC/PXL_20250613_050406028 1.png',
       body: 'e-SportsのためのPC機器類を設置。eDCタワー6F (図書館内) にあります。' },
-    { title: 'HIUファブ', img: 'MCC/PXL_20250613_050629025 1.png',
+    { title: 'HIUファブ', img: '${base}/MCC/PXL_20250613_050629025 1.png',
       body: '3Dプリンタやレーザー加工機などのデジタルファブリケーション機器を設置。Gスタジオ内にあります。' }
   ];
 </script>
@@ -208,7 +208,7 @@
     <!-- 左側 -->
     <div class="footer-left">
       <div class="brand-row">
-        <img src="MCC/MCCrogo.png" alt="HIU Media Creative Center" class="brand-mcc" />
+        <img src="/MCC/MCCrogo.png" alt="HIU Media Creative Center" class="brand-mcc" />
       </div>
 
       <address class="footer-addr">
@@ -220,8 +220,8 @@
 
     <!-- 右側 -->
     <div class="footer-right">
-      <a href=”https://www.do-johodai.ac.jp/”><img src="/MCC/HIUrogo.png" alt="北海道情報大学 ロゴ" class="hiu-logo"></a>
-      <img src="MCC/kage.png" alt="北海道情報大学 ロゴ" class="hiu-logo2">
+      <a href="https://www.do-johodai.ac.jp/"><img src="{base}/MCC/HIUrogo.png" alt="北海道情報大学 ロゴ" class="hiu-logo"></a>
+      <img src="{base}/MCC/kage.png" alt="北海道情報大学 ロゴ" class="hiu-logo2">
     </div>
   </div>
 
