5bce4f891ceefd5a8176a80d9ad073f1969b8e86
Tukimiya0317 <dod32013@gmail.com>
2025-12-21 21:24:07 +0900

    画像修正中２

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
+const dev = process.env.NODE_ENV === 'development';
 
 export default {
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter({
-      strict: false
-    }),
-
+    adapter: adapter({ strict: false }),
     paths: {
-      base: dev ? "" : "/mcc-website2025",
-      assets: ""
+      base: dev ? '' : '/mcc-website2025'
     },
-
     prerender: {
-      entries: ["/"],   // ★ 最重要：index を明示的に指定する
-      handleHttpError: "ignore",
-      handleUnseenRoutes: "ignore"
+      entries: ['/'],
+      handleHttpError: 'ignore',
+      handleUnseenRoutes: 'ignore'
     }
   }
 };
+
